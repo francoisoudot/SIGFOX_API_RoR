b01955cf7d43db33c8122e95fc1b10cb11ab6f62
francoisoudot <francois.oudot@gmail.com>
2015-04-03 18:40:46 -0700

    DL added

--- app/controllers/sigfox_controller.rb
+++ app/controllers/sigfox_controller.rb
@@ -4,11 +4,26 @@ class SigfoxController < ApplicationController
   end
 
   def devicetype
+    
+    device_id=params['id']
+
    Devicetype.create(
-   	:device_id=>params['id'],
+   	:device_id=>device_id,
    	:time=>params['time'],
    	:data=>params['data'],
    	:rssi=>params['rssi'],
    	:signal=>params['signal'])
+
+   /JSON answer to the DL/
+
+    if params['ack']=="true"
+      /change the data to send back/
+      render :json=>{
+        device_id => { "downlinkData" => "deadbeefcafebabe"}
+      }
+    else
+      render :json=>''
+    end
+
   end
 end
